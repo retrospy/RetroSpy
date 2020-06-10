8bd5e1bac522484e7f379832312183c42712f320
edru <edwardarubio@gmail.com>
2020-06-10 15:14:41 -0500

    Create update-retrospy.sh
    End user MiSTer script to download update-retrospy-installer.sh.

---
+++ MiSter/update-retrospy.sh
@@ -0,0 +1,16 @@
+echo "Installing RetroSpy for MiSTer"
+echo " "
+
+echo "Updating and executing the RetroSpy installer script."
+echo " "
+
+wget -q -t 3 --output-file=/tmp/wget-log --show-progress -O /tmp/update-retrospy-installer.sh https://github.com/retrospy/RetroSpy/tree/master/MiSter/update-retrospy-installer.sh
+
+chmod +x /tmp/update-retrospy-installer.sh
+
+/tmp/update-retrospy-installer.sh
+
+rm /tmp/update-retrospy-installer.sh
+
+echo " " 
+echo "Installation complete! Please go to http://www.retro-spy.com to download the newest Windows client application."
