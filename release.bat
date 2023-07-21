1cbb1d2abbb49670b77feb2e3ce94436b068d252
Christopher J Mallery <zoggins@retro-spy.com>
2023-07-20 18:22:02 -0700

    Changed signtool command for installer

--- release.bat
+++ release.bat
@@ -191,9 +191,8 @@ REM Copy Drivers
 
 if exist "C:\Program Files (x86)\Actual Installer\actinst.exe" (
 "C:\Program Files (x86)\Actual Installer\actinst.exe" /S ".\RetroSpy-64.aip"
-  if exist "..\..\..\certs\codesign.pfx" (
-    "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0\x86\SignTool" sign /f "..\..\..\certs\codesign.pfx" /p %codesignpasswd% /tr http://timestamp.comodoca.com  /td sha256 /fd sha256 /a Retrospy-Setup.exe
-  )
+  if exist "..\..\..\certs\codesign.cer" (
+    "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0\x86\SignTool" sign /f "..\..\..\..\..\..\certs\codesign.cer" /csp "eToken Base Cryptographic Provider" /k "%codesignpasswd%" /tr http://timestamp.comodoca.com  /td sha256 /fd sha256 /a Retrospy-Setup.exe)
 )
 
 ;mkdir RetroSpy-Upload
