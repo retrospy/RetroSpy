aa7a15a9e078d195159129d1647e70bc1357200e
Christopher J Mallery <zoggins@retro-spy.com>
2023-07-21 13:08:42 -0700

    Updating version to 6.3

--- firmware/sketches/ControllerSpy.h
+++ firmware/sketches/ControllerSpy.h
@@ -44,7 +44,7 @@ public:
 		Serial.print(startupMsg());
 		Serial.println(" mode");
 		Serial.print("// Version: ");
-		Serial.println("6.2"); /*VERSIONINFO*/
+		Serial.println("6.3"); /*VERSIONINFO*/
 		delay(1000);		
 	}
 	
