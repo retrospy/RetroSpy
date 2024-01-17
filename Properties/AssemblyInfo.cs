c72df0e330149e905a95ccefd451ba39c8716d6d
Christopher J Mallery <zoggins@retro-spy.com>
2024-01-17 11:01:51 -0800

    Updated 5.9 with newer GBP software

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -49,7 +49,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.2.3")]
-[assembly: AssemblyFileVersion("5.2.3")]
+[assembly: AssemblyVersion("5.9.0")]
+[assembly: AssemblyFileVersion("5.9.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
