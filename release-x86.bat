ad5d112bf453c094324b7810b64a4effe6718724
Christopher J Mallery <zoggins@retro-spy.com>
2024-08-19 20:37:21 -0700

    Trying to fix 32-bit windows 10

--- release-x86.bat
+++ release-x86.bat
@@ -5,16 +5,16 @@ IF NOT "%~1"=="" set sub=1
 
 :release
 rmdir /S /Q bin\Release\net8.0
-"C:\Program Files\dotnet\dotnet.exe" build RetroSpyX\RetroSpyX.csproj /p:RuntimeIdentifier=win-x86 /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=..\bin\Release\net8.0
+"C:\Program Files\dotnet\dotnet.exe" build RetroSpyX\RetroSpyX.csproj --runtime win-x86 /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=..\bin\Release\net8.0
 if %ERRORLEVEL% NEQ 0 goto :fail
 
-"C:\Program Files\dotnet\dotnet.exe" build GBPemuX\GBPemuX.csproj /p:RuntimeIdentifier=win-x86 /p:Configuration=Release /p:Platform="Any CPU"  /p:OutputPath=..\bin\Release\net8.0
+"C:\Program Files\dotnet\dotnet.exe" build GBPemuX\GBPemuX.csproj --runtime win-x86 /p:Configuration=Release /p:Platform="Any CPU"  /p:OutputPath=..\bin\Release\net8.0
 if %ERRORLEVEL% NEQ 0 goto :fail
 
-"C:\Program Files\dotnet\dotnet.exe" build UsbUpdaterX2\UsbUpdaterX2.csproj /p:RuntimeIdentifier=win-x86 /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=..\bin\Release\net8.0
+"C:\Program Files\dotnet\dotnet.exe" build UsbUpdaterX2\UsbUpdaterX2.csproj --runtime win-x86 /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=..\bin\Release\net8.0
 if %ERRORLEVEL% NEQ 0 goto :fail
 
-"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\amd64\MSBuild.exe" GBPUpdaterX2\GBPUpdaterX2.csproj /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=..\bin\Release\net8.0
+"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\amd64\MSBuild.exe" GBPUpdaterX2\GBPUpdaterX2.csproj /p:TargetFrameworks=net8.0 /p:RuntimeIdentifiers=win-x86 /p:Configuration=Release /p:Platform="Any CPU" /p:OutputPath=..\bin\Release\net8.0
 if %ERRORLEVEL% NEQ 0 goto :fail
 
 cd MiSTer
