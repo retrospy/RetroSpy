f652fad9ee03015a09a708143505a14a76eecf3b
Christopher J Mallery <zoggins@retro-spy.com>
2025-01-30 19:46:41 -0800

    Bumping to 6.8.1

--- firmware/sketches/ControllerSpy.h
+++ firmware/sketches/ControllerSpy.h
@@ -52,7 +52,7 @@ public:
 		{
 			Serial.println("// Selected mode is unsupported on this hardware");
 		}
-		Serial.println("6.8"); /*VERSIONINFO*/
+		Serial.println("6.8.1"); /*VERSIONINFO*/
 		delay(1000);
 	}
 		
